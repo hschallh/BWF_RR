7731105ca5fa9607f4d125860803ee37a54db89a
hschallh <hschallh@gmail.com>
2018-01-26 13:48:24 -0500

    Working on Workouts
    Created a reccomended routine workout that can parse a text file and
    create a workout with warmups and sets of progressions with exercises.
    Also added a .gitignore for testing. Need to parse from a pebble
    resource file next. Also, should there be a progression routine
    for a group of progressions (skill and strength)?

--- src/c/workout.c
+++ src/c/workout.c
@@ -1,29 +1,102 @@
-#include <pebble.h>
 #include "workout.h"
+#include <pebble.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Create a workout from 6 exercises
-// @param  A1 The first exercise in the first exercise pair
-// @param  B1 The second exercise in the first exercise pair
-// @param  A2 The first exercise in the second exercise pair
-// @param  B2 The second exercise in the second exercise pair
-// @param  A3 The first exercise in the third exercise pair
-// @param  B3 The second exercise in the third exercise pair
+// Create and build progressions and their exercises from a string
+// @param progressions The array of progressions to build
+// @param progArrayLen The length of the progressions array
+// @param str Pointer to the progressions string. Should be of the
+//            form <progression1>+<progression2>+...+<progression6>
+void parseProgressionsForWorkout(Progression* progressions[],
+                                 char* progressionsStr) {
+  // Find the signifier for the next progression in the list
+  char* progressionSignifier = strstr(progressionsStr, "+");
+  int progressionNum = 0;
+
+  while (progressionSignifier != NULL) {
+    // Null terminate the progression string, parse and add to the
+    // progression array
+    progressionSignifier[0] = '\0';
+    progressions[progressionNum++] = parseProgression(progressionsStr);
+
+    // Move to the next progression
+    progressionsStr = progressionSignifier + 1;
+    progressionSignifier = strstr(progressionsStr, "+");
+  }
+
+  // Add the last progression to the array
+  progressions[progressionNum] = parseProgression(progressionsStr);
+}
+
+// Parse and entire BWF RR from a file
+void parseWorkout(Workout* workout, char* filename) {
+  // Open the workout file
+  FILE* workoutFile = fopen(filename, "r");
+  char buf[999];
+
+  // Read the first line and parse the Stretching Exercises
+  fgets(buf, sizeof(buf), workoutFile);
+  workout->stretches = createWarmup("Dynamic Stretches", 10, "reps");
+  parseExercisesForWarmup(workout->stretches, buf);
+
+  // Read the second line and parse the Bodyline Exercises
+  fgets(buf, sizeof(buf), workoutFile);
+  workout->bodylineDrills = createWarmup("Bodyline Drills", 60, "secs");
+  parseExercisesForWarmup(workout->bodylineDrills, buf);
+
+  // Read the third line and parse the Skill Exercises
+  fgets(buf, sizeof(buf), workoutFile);
+  parseProgressionsForWorkout(workout->skillProgressions, buf);
+
+  // Read the fourth line and parse the Strength Exercises
+  fgets(buf, sizeof(buf), workoutFile);
+  parseProgressionsForWorkout(workout->strengthProgressions, buf);
+
+  // Close the workout file
+  fclose(workoutFile);
+}
+
+// Build a workout plan
+// @param progNums An array of numbers representing where the user is in each
+//                  progression
+// @param filename A file containing 6 progressions and their respective
+//                  exercises
 // @return The newly created workout
-Workout* createWorkout(Exercise* A1, Exercise* B1, Exercise* A2, Exercise* B2, Exercise* A3, Exercise* B3) {
-  Workout* workout = malloc(sizeof(Workout));
-  workout->exercises[0] = A1;
-  workout->exercises[1] = B1;
-  workout->exercises[2] = A2;
-  workout->exercises[3] = B2;
-  workout->exercises[4] = A3;
-  workout->exercises[5] = B3;
-  workout->currentPair = 0;
-  
+Workout* createWorkout(int skillNums[NUM_OF_SKILL_EXERCISES],
+                       int strengthNums[NUM_OF_STRENGTH_EXERCISES]) {
+  // Create a Workout and parse the progressions form the workout file
+  Workout* workout = (Workout*)malloc(sizeof(Workout));
+
+  // Set the exercises the user is to do for their skill work
+  for (int i = 0; i < NUM_OF_SKILL_EXERCISES; i++) {
+    workout->exerciseInSkill[i] = skillNums[i];
+  }
+
+  // Set the exercises the user is to do for their strength work
+  for (int i = 0; i < NUM_OF_STRENGTH_EXERCISES; i++) {
+    workout->exerciseInStrength[i] = strengthNums[i];
+  }
+
   return workout;
 }
 
-// Deallocates memory put aside for the workout
-// @param workout The workout to destroy
+// Free all of the memory allocated to a workout
+// @param wo The workout to destroy
 void destroyWorkout(Workout* workout) {
+  // Free all the strength progressions in this workout
+  for (int i = 0; i < NUM_OF_SKILL_EXERCISES; i++) {
+    destroyProgression(workout->skillProgressions[i]);
+  }
+  // Free all the strength progressions in this workout
+  for (int i = 0; i < NUM_OF_STRENGTH_EXERCISES; i++) {
+    destroyProgression(workout->strengthProgressions[i]);
+  }
+
+  destroyWarmup(workout->stretches);
+  destroyWarmup(workout->bodylineDrills);
+
+  // Free the workout
   free(workout);
 }
--- src/c/workout.h
+++ src/c/workout.h
@@ -1,15 +1,26 @@
 #pragma once
 
-#include "progression.h"
 #include <stdlib.h>
+#include "progression.h"
+#include "warmup.h"
+
+#define NUM_OF_STRETCHES 9
+#define NUM_OF_BODYLINE_DRILLS 6
+#define NUM_OF_STRENGTH_EXERCISES 6
+#define NUM_OF_SKILL_EXERCISES 3
 
 typedef struct Workout Workout;
-  
+
 struct Workout {
-  Exercise* exercises[6];
-  int currentPair;
+  Warmup* stretches;
+  Warmup* bodylineDrills;
+  int exerciseInSkill[NUM_OF_SKILL_EXERCISES];
+  Progression* skillProgressions[NUM_OF_SKILL_EXERCISES];
+  int exerciseInStrength[NUM_OF_STRENGTH_EXERCISES];
+  Progression* strengthProgressions[NUM_OF_STRENGTH_EXERCISES];
 };
 
-Workout* buildWorkout();
-Workout* createWorkout(Exercise* A1, Exercise* B1, Exercise* A2, Exercise* B2, Exercise* A3, Exercise* B3);
+Workout* createWorkout(int skillNums[NUM_OF_SKILL_EXERCISES],
+                       int strengthNums[NUM_OF_STRENGTH_EXERCISES]);
+void parseWorkout(Workout* workout, char* filename);
 void destroyWorkout(Workout* workout);
